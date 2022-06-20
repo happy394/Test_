9a007a7992436b588ac5834886c33baad85d3312
happy394 <106350498+happy394@users.noreply.github.com>
2022-06-20 22:14:03 +0300

    Bug fixed in Laza.cpp
    Now the program doesnt return "Error". 
    Also, added some comments to explain how the code works.

--- Laza.cpp
+++ Laza.cpp
@@ -6,45 +6,42 @@ using namespace std;
 struct students { // Тип данных (int)
     bool gender; // мальчик = 1. девочка = 0.
     string name;
-    int mark; // Пусть будут от 1 до 5 
+    int mark; // 5-и бальная система
 };
 
 struct buff {
-    int mark = 0;
-    int i;
+    int mark = 0; // ставим минимальную, чтобы любая отметка ученика в последствии заменила эту переменную
+    int i; // принимаетя положение, как и в массиве, чтобы сразу вывести имя в ответе
 };
 
 int main() {
-    int n;
+    int n; // кол-во учеников
     cin >> n;
-    buff buff_m;
-    buff buff_f;
-    students array[100];
-    string s = "John";
-    //cout << s;
-    /*cin >> array[i].gender >> array[i].name >> array[i].mark;
-    cout << array[i].gender << array[i].name << array[i].mark;*/
-    for (int i = 0; i < n; i++) {
+    buff buff_m; // мальчики
+    buff buff_f; // девочки
+    students array[100]; //массив со всеми учениками и их данными
+    for (int i = 0; i < n; i++) {                                       // прогон всего массива с учениками students array[100]
         cin >> array[i].gender >> array[i].name >> array[i].mark;
-        //s = array[i].name;
-        //cout << s;
-        if (array[i].gender == 1) {
-            if (buff_m.mark > array[i].mark) {
+        if (array[i].gender == 1) {                                     // вычисление лучшей отметки у мальчиков
+            if (buff_m.mark < array[i].mark) {
                 buff_m.i = i;
                 buff_m.mark = max(buff_m.mark, array[i].mark);
             }
         }
-        if (array[i].gender == 0) {
-            if (buff_f.mark > array[i].mark) {
+        if (array[i].gender == 0) {                                     // вычисление лучшей отметки у девочек
+            if (buff_f.mark < array[i].mark) {
                 buff_f.i = i;
                 buff_f.mark = max(buff_f.mark, array[i].mark);
             }
         }
-        
+
     }
-    cout << buff_m.i;
-    s = array[buff_m.i].name;
-    cout << s << array[buff_m.i ].mark << endl;
-    /*cout << array[buff_f.i].name << array[buff_f.mark].mark;*/
+    cout << endl << "The best students:" << endl;
+    cout << array[buff_m.i].name << ",  mark: " <<array[buff_m.i].mark << endl;
+    cout << array[buff_f.i].name << ",  mark: " << array[buff_f.i].mark << endl;
     return 0;
 }
+
+//Для теста программы можно взять эти данные:
+// 1 John 3   1 Erik 5   0 Adel 2   0 Eva 5
+// Можно просто скопировать и вставить, не забыв упомянуть, что учеников 4
